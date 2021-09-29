e3e8b0b3814b25537e413cc4be2377f04b58e68a
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-09-30 01:54:21 +0300

    Upload project

---
+++ src/main/java/com/ingbootcamp/springmicroserviceproject/SpringMicroserviceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ingbootcamp.springmicroserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingbootcamp/springmicroserviceproject/SpringMicroserviceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingbootcamp.springmicroserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
