c64c716a035b9d1e7c1f5d4a4e521b09ec3e68f8
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-10-01 00:48:37 +0300

    Edit AccountDto getter/setters

--- service-common/src/main/java/com/ingbootcamp/servicecommon/contracts/AccountDto.java
+++ service-common/src/main/java/com/ingbootcamp/servicecommon/contracts/AccountDto.java
@@ -1,8 +1,12 @@
 package com.ingbootcamp.servicecommon.contracts;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDto {
     private String id;
     private String name;
