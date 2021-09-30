6ea381759bc286c4b1af8a6d24c3b15f480ffc0e
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-09-30 13:21:20 +0300

    Add Account service and Service parent modules

---
+++ account-service/src/main/java/com/ingbootcamp/accountservice/entity/Account.java
@@ -0,0 +1,20 @@
+package com.ingbootcamp.accountservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    
+    private String id;
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+    private Date created_at;
+}
