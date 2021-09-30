2758cd90e1b49fe2d4a41aa2a904e4ab661c1f5f
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-09-30 20:31:20 +0300

    Add AccountApi endpoints

--- account-service/src/main/java/com/ingbootcamp/accountservice/api/AccountApi.java
+++ account-service/src/main/java/com/ingbootcamp/accountservice/api/AccountApi.java
@@ -1,4 +1,57 @@
 package com.ingbootcamp.accountservice.api;
 
+import com.ingbootcamp.accountservice.business.abstracts.AccountService;
+import com.ingbootcamp.accountservice.core.results.DataResult;
+import com.ingbootcamp.accountservice.core.results.Result;
+import com.ingbootcamp.accountservice.core.results.SuccessDataResult;
+import com.ingbootcamp.accountservice.dto.AccountDto;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("accounts")
 public class AccountApi {
+    private AccountService accountService;
+
+    @Autowired
+    public AccountApi(AccountService accountService){
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public ResponseEntity<DataResult<List<AccountDto>>> getAll(){
+        DataResult<List<AccountDto>> result = this.accountService.getAll();
+        return ResponseEntity.ok(result);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataResult<AccountDto>> get(@PathVariable String id){
+        DataResult<AccountDto> result = accountService.get(id);
+        return ResponseEntity.ok(result);
+    }
+
+    public ResponseEntity<Result> add(@RequestBody AccountDto accountDto){
+        Result addResult = accountService.save(accountDto);
+        return ResponseEntity.ok(addResult);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Result> delete(@RequestParam(value = "id") String id){
+        Result deleteResult = accountService.delete(id);
+        return ResponseEntity.ok(deleteResult);
+    }
+
+    @PutMapping
+    public ResponseEntity<Result> update(@RequestBody AccountDto accountDto){
+        Result updateResult = accountService.update(accountDto);
+        return ResponseEntity.ok(updateResult);
+    }
+
+
+
 }
