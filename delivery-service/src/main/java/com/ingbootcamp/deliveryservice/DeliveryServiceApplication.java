3199d186f111b40dab1aa03d711fcf82b4b33f3f
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-10-01 21:42:03 +0300

    Add delivery-service

---
+++ delivery-service/src/main/java/com/ingbootcamp/deliveryservice/DeliveryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.ingbootcamp.deliveryservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DeliveryServiceApplication {
+
+
+}
