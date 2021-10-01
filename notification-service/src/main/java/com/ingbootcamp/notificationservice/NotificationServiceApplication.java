2af18b3b4139e0bf110d8aa12abe81998bf461c9
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-10-02 01:35:11 +0300

    Edit NotificationService

--- notification-service/src/main/java/com/ingbootcamp/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/ingbootcamp/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,10 @@ package com.ingbootcamp.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ingbootcamp")
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class,args);
