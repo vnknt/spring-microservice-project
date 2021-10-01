0ba6af7c4e88113827d014755e4546e4c66512a0
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-10-01 13:04:42 +0300

    Edit shipment-service configurations for eureka and config server

--- shipment-service/src/main/java/com/ingbootcamp/shipmentservice/ShipmentServiceApplication.java
+++ shipment-service/src/main/java/com/ingbootcamp/shipmentservice/ShipmentServiceApplication.java
@@ -2,10 +2,12 @@ package com.ingbootcamp.shipmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEurekaClient
 public class ShipmentServiceApplication {
     public static void main(String[] args){
         SpringApplication.run(ShipmentServiceApplication.class ,args);
--- shipment-service/src/main/java/com/ingbootcamp/shipmentservice/entity/Shipment.java
+++ shipment-service/src/main/java/com/ingbootcamp/shipmentservice/entity/Shipment.java
@@ -1,12 +1,16 @@
 package com.ingbootcamp.shipmentservice.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 
 @Document
+@Data
 public class Shipment {
+    @Id
     private String id;
     private String sender;
     private String reciever;
