73c518ed85e8301878571a1720d619230ba34615
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-10-01 23:37:50 +0300

    Add Delivery Api methods

--- delivery-service/src/main/java/com/ingbootcamp/deliveryservice/api/DeliveryApi.java
+++ delivery-service/src/main/java/com/ingbootcamp/deliveryservice/api/DeliveryApi.java
@@ -8,6 +8,7 @@ import com.ingbootcamp.servicecommon.results.Result;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("deliveries")
 public class DeliveryApi {
-    
+
     private final DeliveryService deliveryService;
 
     @GetMapping
@@ -27,6 +28,7 @@ public class DeliveryApi {
 
     }
 
+    @PostMapping
     public ResponseEntity<Result> add(DeliveryDto deliveryDto){
         return ResponseEntity.ok(deliveryService.add(deliveryDto));
     }
