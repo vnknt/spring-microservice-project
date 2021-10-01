75120bfbd90f36041bc60fe9d4aea5baf2d67423
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-10-01 23:36:59 +0300

    Add CustomBeanFactory

---
+++ delivery-service/src/main/java/com/ingbootcamp/deliveryservice/config/CustomBeanFactory.java
@@ -0,0 +1,13 @@
+package com.ingbootcamp.deliveryservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomBeanFactory {
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+}
