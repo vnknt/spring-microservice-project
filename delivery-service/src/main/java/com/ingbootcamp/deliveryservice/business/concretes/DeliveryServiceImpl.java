8a6b6a4a827633fc13154cee1f347180a13f27a4
Velat Necmettin Kanat <kanatvn@gmail.com>
2021-10-01 23:39:14 +0300

    Add DeliveryService business methods

--- delivery-service/src/main/java/com/ingbootcamp/deliveryservice/business/concretes/DeliveryServiceImpl.java
+++ delivery-service/src/main/java/com/ingbootcamp/deliveryservice/business/concretes/DeliveryServiceImpl.java
@@ -1,28 +1,51 @@
 package com.ingbootcamp.deliveryservice.business.concretes;
 
 import com.ingbootcamp.deliveryservice.business.abstracts.DeliveryService;
+import com.ingbootcamp.deliveryservice.entity.Delivery;
+import com.ingbootcamp.deliveryservice.repository.DeliveryRepository;
+import com.ingbootcamp.deliveryservice.utilities.DeliveryConverter;
 import com.ingbootcamp.servicecommon.contracts.DeliveryDto;
-import com.ingbootcamp.servicecommon.results.DataResult;
-import com.ingbootcamp.servicecommon.results.Result;
+import com.ingbootcamp.servicecommon.results.*;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@RequiredArgsConstructor
 public class DeliveryServiceImpl implements DeliveryService {
+
+    private final DeliveryRepository deliveryRepository;
+    private final ModelMapper modelMapper;
+    private final DeliveryConverter deliveryConverter;
 
     @Override
     public DataResult<List<DeliveryDto>> getAll() {
-        return null;
+        List<Delivery> deliveries = deliveryRepository.findAll();
+        return new SuccessDataResult<>(deliveryConverter.deliveriesToList(deliveries));
     }
 
     @Override
     public DataResult<DeliveryDto> get(String id) {
-        return null;
+        DeliveryDto deliveryDto ;
+        try{
+            Delivery delivery = deliveryRepository.getById(id);
+            deliveryDto = modelMapper.map(delivery,DeliveryDto.class);
+        }catch (Exception e){
+            return new ErrorDataResult<>("An error occured while getting delivery object");
+        }
+        return new SuccessDataResult<>(deliveryDto);
     }
 
     @Override
     public Result add(DeliveryDto deliveryDto) {
-        return null;
+        try{
+            Delivery delivery = modelMapper.map(deliveryDto,Delivery.class);
+            deliveryRepository.save(delivery);
+        }catch (Exception e){
+            return new ErrorResult("An error occured while addng delivery object");
+        }
+        return new SuccessResult();
     }
 
     @Override
